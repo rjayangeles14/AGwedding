5ed3ede707f1a9691be0d12dded024780439588c
rjayangeles14 <rjayangeles14@gmail.com>
2020-09-22 12:32:15 +0800

    HomePage
    Home Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(window).on("load",function(){
+	// home section slideshow
+	let slideIndex = $(".slide.active").index();
+	const slideLen = $(".slide").length;
+
+
+	function slideShow(){
+		$(".slide").removeClass("active").eq(slideIndex).addClass("active");
+		if(slideIndex == slideLen-1){
+			slideIndex = 0;
+		}
+		else{
+			slideIndex++;
+		}
+		setTimeout(slideShow,5000);
+	}
+
+	slideShow();
+})
