38a69ab5b583ac23183c0ab7107b5883b1a24324
rjayangeles14 <rjayangeles14@gmail.com>
2020-09-27 00:21:49 +0800

    Responsive
    Commit responsive

--- js/main.js
+++ js/main.js
@@ -19,6 +19,33 @@ $(window).on("load",function(){
 })
 
 $(document).ready(function(){
+
+	// nav toggle
+	$(".hamburger-btn").click(function(){
+		$(".header .nav").slideToggle();
+	})
+
+	$(".header .nav a").click(function(){
+		if($(window).width() < 768){
+			$(".header .nav").slideToggle();
+		}
+	})
+
+	// fixed header
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100){
+			$(".header").addClass("fixed");
+		}
+		else{
+			$(".header").removeClass("fixed");
+		}
+	})
+
+	// scrollIt
+	$.scrollIt({
+		topOffset: -50
+	});
+
 	// people filter
 	peopleFilter($(".filter-btn.active").attr("data-target"))
 	$(".filter-btn").click(function(){
@@ -36,4 +63,63 @@ $(document).ready(function(){
 		$(".people-item").hide();
 		$(".people-item[data-category='"+target+"']").fadeIn();
 	}
+
+	// gallery popup
+	const wHeight = $(window).height();
+	$(".gallery-popup .gp-img").css("max-height",wHeight + "px");
+
+	let itemIndex = 0;
+	const totalGalleryItems = $(".gallery-item").length;
+
+	$(".gallery-item").click(function(){
+		itemIndex = $(this).index();
+		$(".gallery-popup").addClass("open");
+		$(".gallery-popup .gp-img").hide();
+		gpSlideShow();
+	})
+
+	$(".gp-controls .next").click(function(){
+		if(itemIndex === totalGalleryItems-1){
+			itemIndex = 0;
+		}
+		else{
+			itemIndex++;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function(){
+			gpSlideShow();
+		})
+	})
+
+	$(".gp-controls .prev").click(function(){
+		if(itemIndex === 0){
+			itemIndex = totalGalleryItems-1;
+		}
+		else{
+			itemIndex--;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function(){
+			gpSlideShow();
+		})
+	})
+
+	function gpSlideShow(){
+		const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+		// console.log(imgSrc);
+		$(".gallery-popup .gp-img").fadeIn().attr("src",imgSrc);
+		$(".gp-counter").text((itemIndex+1) +"/"+ totalGalleryItems);
+	}
+
+	// close gallery popup
+	$(".gp-close").click(function(){
+		$(".gallery-popup").removeClass("open");
+	})
+
+	// hide gallery popup when clicked outside of gp-container
+	$(".gallery-popup").click(function(event){
+		// console.log(event.target);
+		if($(event.target).hasClass("open")){
+			$(".gallery-popup").removeClass("open");
+		}
+	})
+
 })
